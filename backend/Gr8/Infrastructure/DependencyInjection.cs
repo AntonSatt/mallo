7b954864f4a8b0e9e1fb4670b9296df5c3946524
sebastian@temtron.se <sebastian@temtron.se>
2026-03-30 09:51:46 +0200

    Add clean architecture structure

---
+++ backend/Gr8/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gr8.Infrastructure
+{
+    internal class DependencyInjection
+    {
+    }
+}
---
+++ backend/Gr8/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gr8.Infrastructure.Persistence
+{
+    internal class CommunityDbContext : DbContext
+    {
+
+    }
+}

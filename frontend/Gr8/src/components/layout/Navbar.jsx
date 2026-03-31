26c522f873db4585fc522403e8789434201ecef3
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-31 13:20:31 +0200

    changed to Navlink and created .css

--- frontend/Gr8/src/components/layout/Navbar.jsx
+++ frontend/Gr8/src/components/layout/Navbar.jsx
@@ -1,41 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav style={styles.nav}>
-            <div style={styles.logo}>
-                <Link to='/' style={styles.link}>GR8 COMMUNITY</Link>
+        <nav>
+            <div>
+                <NavLink to="/">GR8 COMMUNITY</NavLink>
             </div>
-            <ul style={styles.navLinks}>
-                <li><Link to='/' style={styles.link}>Hem</Link></li>
-                <li><Link to='/login' style={styles.link}>Logga in</Link></li>
-                <li><Link to='/register' style={styles.link}>Registrering</Link></li>
+
+            <ul>
+                <li><NavLink to="/">Hem</NavLink></li>
+                <li><NavLink to="/login">Logga in</NavLink></li>
+                <li><NavLink to="/register">Registrering</NavLink></li>
             </ul>
         </nav>
     )
-}
-
-/* Super simple design. delete this later. */
-const styles = {
-    nav: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '1rem 2rem',
-        background: '#333',
-        color: '#fff'
-    },
-    navLinks: {
-        display: 'flex',
-        listStyle: 'none',
-        gap: '15px',
-    },
-    link: {
-        color: '#fff',
-        textDecoration: 'none',
-        fontWeight: 'bold'
-    }
 }
 
 export default Navbar;

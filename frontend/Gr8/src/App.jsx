f1b477ca6efb5edadc04574ff09b21ddff8129d2
Daniel Hultmark <danielhultmark@gmail.com>
2026-04-01 15:06:00 +0200

    Created Settings page with delete function also added Material

--- frontend/Gr8/src/App.jsx
+++ frontend/Gr8/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import Settings from './pages/Settings'
 
 function App() {
  
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/settings' element={<Settings />} />
       </Routes>
     </Router>
      
---
+++ frontend/Gr8/src/components/settings/UserSettings.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import { Dialog } from "@mui/material";
+import { DialogTitle } from "@mui/material";
+import { DialogActions } from "@mui/material";
+
+const UserSettings = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event) => {
+        setOpen(false);
+        console.log(event.target.id);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Inställningar sparade!");
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <h2>Användarinställningar</h2>
+                <label htmlFor="username">
+                    Användarnamn:
+                    <input type="text" id="username" name="username" />
+                </label>
+                <label htmlFor="name">
+                    Namn:
+                    <input type="text" id="name" name="name" />
+                </label>
+                <label htmlFor="email">
+                    Email:
+                    <input type="email" id="email" name="email" />
+                </label>
+                <label htmlFor="password">
+                    Lösenord:
+                    <input type="password" id="password" name="password" />
+                </label>
+                <button type="submit">Spara inställningar</button>
+
+                <button type="delete" onClick={handleClickOpen}>
+                    Ta bort konto
+                </button>
+            </form>
+            
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Är du säker på att du vill ta bort ditt konto?</DialogTitle>
+                <DialogActions>
+                    <button onClick={handleClose}>Avbryt</button>
+                    <button onClick={handleClose} id="deleteUser">Ja, ta bort konto</button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
+
+export default UserSettings;

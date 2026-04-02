da8bad35a859552d3350815abd7ba2d5be153149
sebastian@temtron.se <sebastian@temtron.se>
2026-04-02 12:39:06 +0200

    Changed UserServices to async and added register/logout/delete endpoints, store token in localstorage

--- frontend/Gr8/src/services/UserServices.jsx
+++ frontend/Gr8/src/services/UserServices.jsx
@@ -1,13 +1,33 @@
 import ApiClient from "../api/ApiClient";
 
 const UserServices = {
-    login: (userName, password) => {
-        ApiClient.post("/login", {
+    login: async (userName, password) => {
+        const response = await ApiClient.post("/login", {
             userName: userName,
             password: password
-        }).then((response) => {
-            console.log(response);
         });
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        return response.data;
+    },
+    register: async (userName, name, email, password) => {
+        const response = await ApiClient.post("/register", {
+            userName: userName,
+            name: name,
+            email: email,
+            password: password
+        });
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        return response.data;
+    },
+    logout: () => {
+        localStorage.removeItem("token");
+    },
+    delete: async (userName) => {
+        const response = await ApiClient.delete("/delete", userName);
+        this.logout();
+        return response.data;
     }
 };
 

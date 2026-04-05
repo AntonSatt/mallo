d215ed505fbb257d7d62dd18f5bb543d1d9b0c1b
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-04-05 19:00:22 +0200

    Add useState validation and error handling in LoginForm-frontend

--- frontend/Gr8/src/components/LoginForm/LoginForm.jsx
+++ frontend/Gr8/src/components/LoginForm/LoginForm.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import UserServices from "../../services/UserServices";
 
 const LoginForm = () => {
-  const handleSubmit = async (event) => { //TODO: Change to useeffect?
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    UserServices.login(event.target.userName.value, event.target.password.value);
+
+    if (!userName || !password) 
+    {
+      setError("Username and password are required")
+      return;
+    }
+
+    setError("");
+    await UserServices.login(userName, password);
   };
 
   return (
@@ -14,19 +27,23 @@ const LoginForm = () => {
         <fieldset>
           <p>
             <label htmlFor="userName">Användarnamn </label>
-            <input type="text" id="userName" />
+            <input type="text" id="userName" name="userName"
+            value={userName} onChange={(event) => setUserName(event.target.value)} /> {/* controlled inputs*/}
           </p>
 
           <p>
             <label htmlFor="password">Lösenord </label>
-            <input type="text" id="password" />
+            <input type="password" id="password" name="password" 
+            value={password} onChange={(event) => setPassword(event.target.value)}/>
           </p>
+
+          {error && <p>{error}</p>} 
 
           <button type="submit">Logga in</button>
         </fieldset>
 
         <h3>Glömt ditt lösenord?</h3>
-        <button type="submit">Klicka här</button>
+        <button type="button">Klicka här</button>
       </form>
     </section>
   );

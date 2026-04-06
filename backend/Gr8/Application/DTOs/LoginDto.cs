0b44b518554c07a17c6f95b07edd53aa1adf256b
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-04-06 20:01:58 +0200

    Update registerDto

--- backend/Gr8/Application/DTOs/LoginDto.cs
+++ backend/Gr8/Application/DTOs/LoginDto.cs
@@ -8,9 +8,9 @@ namespace Gr8.Application.DTOs
     public class LoginDto
     {
         [Required(ErrorMessage = "Username is required")]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }

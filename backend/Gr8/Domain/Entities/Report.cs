cf374598ec486ec9d8ab5c13391787bf111d13d2
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-04-10 14:37:25 +0200

    Created Report entity

---
+++ backend/Gr8/Domain/Entities/Report.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gr8.Domain.Entities
+{
+    public class Report
+    {
+        public int Id { get; set; }
+
+        public string ReportedByUserId { get; private set; } = null!;
+
+        public int? PostId { get; set; }
+        public Post? Post { get; set; }
+
+        public int CommentId { get; set; }
+        public Comment? Comment { get; set; }
+
+        public string Reason { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string? ReviewedByAdminId { get; set; }
+
+        public DateTime? ReviewedAt { get; set; }
+    }
+}

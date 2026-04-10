40e09989b3819deb6e1193d88f1d18dfa751b625
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-04-10 10:59:37 +0200

    Fixed lines

--- backend/Gr8/Infrastructure/Identity/ApplicationDbContext.cs
+++ backend/Gr8/Infrastructure/Identity/ApplicationDbContext.cs
@@ -14,6 +14,11 @@ namespace Gr8.Infrastructure.Identity
     /// </summary>
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);

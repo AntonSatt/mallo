46f95fb1c79c8004c9aca23d086a7259f793a861
Elina <ellanjonsson@hotmail.com>
2026-04-24 08:51:35 +0200

    Added global theme design and styling

---
+++ frontend/Gr8/src/design/buttons/PrimaryButton.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const PrimaryButton = styled(Button)({
+    borderRadius: "20px",
+    backgroundColor: "var(--color-primary)",
+    color: "#ffffff",
+    width: "100%",
+});
+
+export default PrimaryButton;

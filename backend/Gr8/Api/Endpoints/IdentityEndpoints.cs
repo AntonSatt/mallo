3af3f57dafc9714bc2dc8bc64509d1fc4e09ddeb
Elina <ellanjonsson@hotmail.com>
2026-04-29 11:00:59 +0200

    Implemented backend logic to check that user is over 18. Added static progressBar that registrationPage and steps (1-3) implements

--- backend/Gr8/Api/Endpoints/IdentityEndpoints.cs
+++ backend/Gr8/Api/Endpoints/IdentityEndpoints.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Gr8.Api.Endpoints
 {
@@ -26,6 +27,25 @@ namespace Gr8.Api.Endpoints
                     if (!isValid)
                     {
                         return Results.BadRequest(results);
+                    }
+
+                    if (!DateTime.TryParseExact(userDto.SocialNumber, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var birthDate))
+                    {
+                        return Results.BadRequest(new { Message = "Social security number must be in a valid format, yyyyMMdd" });
+                    }
+
+                    var today = DateTime.Today;
+                    var age = today.Year - birthDate.Year;
+                    var birthdayThisYear = birthDate.AddYears(age);
+                    
+                    if (today < birthdayThisYear)
+                    {
+                        age--;
+                    }
+
+                    if (age < 18)
+                    {
+                        return Results.BadRequest(new {Message = "You must be 18 years or older to register."});
                     }
 
                     var user = new ApplicationUser
--- backend/Gr8/Application/DTOs/RegisterDto.cs
+++ backend/Gr8/Application/DTOs/RegisterDto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace Gr8.Application.DTOs
 {

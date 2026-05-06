17ca884a7e18f4d800f4353f9f5fa9acc9107f66
Daniel Hultmark <danielhultmark@gmail.com>
2026-05-06 11:22:05 +0200

    Test for pipeline

---
+++ frontend/Gr8/src/design/buttons/SettingsButton.jsx
@@ -0,0 +1,15 @@
+import { Button, Box, Grid } from "@mui/material";
+
+const SettingsButtonStyles = ({
+
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: 4,
+    padding: '12px 16px',
+    minWidth: 90,
+    color: 'inherit',
+    backgroundColor: '#FFF0D4',
+    gap: 1,
+    '&:hover': { backgroundColor: '#ffe4b5' }
+});
+export default SettingsButtonStyles;

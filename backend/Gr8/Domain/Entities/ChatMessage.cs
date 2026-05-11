16b66516e9d3a4833b3f9a8a938cae496c3cea2a
Victoria Lindén <victoria.linden93@gmail.com>
2026-05-11 14:00:56 +0200

    Change to string to match IdentityUser

--- backend/Gr8/Domain/Entities/ChatMessage.cs
+++ backend/Gr8/Domain/Entities/ChatMessage.cs
@@ -7,8 +7,8 @@ namespace Gr8.Domain.Entities
     public class ChatMessage
     {
         public int Id { get; set; }
-        public int SenderId { get; set; } 
-        public int ReceiverId { get; set; }
+        public string SenderId { get; set; } = null!;
+        public string ReceiverId { get; set; } = null!;
         public string Content { get; set; } = null!;
         public DateTime SendAt { get; set; } = DateTime.UtcNow;
         public bool IsRead { get; set; } = false;

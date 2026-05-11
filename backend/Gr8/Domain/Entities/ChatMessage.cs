0a1e48a20eb9ebf6c36987d14f1b567adb5a181d
Victoria Lindén <victoria.linden93@gmail.com>
2026-05-11 21:23:45 +0200

    Update ChatMessage-entity, organize Report-entity, modelBuilder for ChatMessage

--- backend/Gr8/Domain/Entities/ChatMessage.cs
+++ backend/Gr8/Domain/Entities/ChatMessage.cs
@@ -7,13 +7,16 @@ namespace Gr8.Domain.Entities
     public class ChatMessage
     {
         public int Id { get; set; }
-        public string SenderId { get; set; } = null!;
-        public string ReceiverId { get; set; } = null!;
         public string Content { get; set; } = null!;
         public DateTime SendAt { get; set; } = DateTime.UtcNow;
         public bool IsRead { get; set; } = false;
+        public string SenderId { get; set; } = null!;
+        public string ReceiverId { get; set; } = null!;
 
-        public int ActivityId { get; set; } //FK
+        public int? ActivityId { get; set; } //FK
         public Activity Activity { get; set; } = null!;
+
+        public bool DeletedBySender { get; set; } = false;
+        public bool DeletedByReceiver { get; set; } = false;
     }
 }

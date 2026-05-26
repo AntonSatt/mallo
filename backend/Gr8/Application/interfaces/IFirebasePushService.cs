5f15d3e1ca83f75dadbdb8cacc6888c00549f36b
Victoria Lindén <victoria.linden93@gmail.com>
2026-05-26 21:03:14 +0200

    Add Firebas notification service and backend firebase SDK integration

---
+++ backend/Gr8/Application/interfaces/IFirebasePushService.cs
@@ -0,0 +1,10 @@
+﻿
+//A contract that NotificationService can use without needing to know how Firebase works.
+
+namespace Gr8.Application.Interfaces
+{
+    public interface IFirebasePushService
+    {
+        Task SendToUserAsync(string userId, string title, string body);
+    }
+}
---
+++ backend/Gr8/Infrastructure/Services/FirebasePushService.cs
@@ -0,0 +1,35 @@
+﻿using FirebaseAdmin.Messaging;
+using Gr8.Application.Interfaces;
+
+namespace Gr8.Infrastructure.Services
+{
+    public class FirebasePushService : IFirebasePushService
+    {
+        private readonly ICommunityRepository _communityRepository;
+
+        public FirebasePushService(ICommunityRepository communityRepository) 
+        {
+            _communityRepository = communityRepository;
+        }
+
+        public async Task SendToUserAsync(string userId, string Title, string body)
+        {
+            var tokens = await _communityRepository.GetFirebaseTokensByUserIdAsync(userId);
+
+            foreach(var firebaseToken in tokens)
+            {
+                var message = new Message
+                {
+                    Token = firebaseToken.Token,
+                    Notification = new Notification
+                    {
+                        Title = Title,
+                        Body = body
+                    }
+                };
+
+                await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            }
+        }
+    }
+}
